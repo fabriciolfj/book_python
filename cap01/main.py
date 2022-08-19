dbbda317096ffa4af7feb9283ce1a84d8c56594d
fabricio jacob <fabricio.jacob@outlook.com>
2022-08-19 07:56:27 -0300

    add

---
+++ cap01/main.py
@@ -0,0 +1,20 @@
+from Vector import Vector
+from french_deck import Card, FrenchDeck
+
+
+beer_card = Card('7', 'diamonds')
+print(beer_card)
+
+deck = FrenchDeck()
+print(len(deck))
+
+
+v1 = Vector(2, 4)
+v2 = Vector(2, 1)
+v3 = v1 + v2
+print(v3)
+
+v4 = v1 * 3
+print(v4)
+print(abs(v1))
+print(bool(v1))
