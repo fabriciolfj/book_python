34dc005481e565d46de8a6973f3d55a4816a9c55
fabricio jacob <fabricio.jacob@outlook.com>
2022-09-06 22:51:23 -0300

    add

--- aula03/calculo_imc.py
+++ aula03/calculo_imc.py
@@ -4,4 +4,4 @@ altura = 1.90
 peso = 120
 e_maior = idade > 18
 imc = peso / (altura * altura)
-print(nome, "tem", idade, "de idade e seu imc é", imc)
+print('{0} tem {1} de idade e seu imc e {2:.2f}'.format(nome, idade, imc))
